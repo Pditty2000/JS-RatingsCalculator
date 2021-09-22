0142bc155b7ec66d865d84013e80d12072119ebe
Johnston, Peter <peter.johnston@wizards.com>
2021-09-22 14:35:51 -0700

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -1,4 +1,4 @@
-const collect_ratings() {
+const collect_ratings = () => {
 	const ratings('count':0, 'sum':0, 'average':0);
 	let rating = 0;
 	const elements = document.querySelectorAll('.rating');
